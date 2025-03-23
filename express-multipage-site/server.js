2fcc1d80db146b6fd8b24684d95c6bc102854857
Mila Abadinas <113806288+Lesyuex-denini@users.noreply.github.com>
2025-03-23 18:22:51 +0800

    first

--- express-multipage-site/server.js
+++ express-multipage-site/server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes for different pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
@@ -20,7 +18,6 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
